2b738da9b871455f41d5730219cfe6a72e40053a
Efe <160046958+Efoli61@users.noreply.github.com>
2024-02-14 21:26:34 +0300

    lesson1.py

--- lesson1.py
+++ lesson1.py
@@ -1,3 +1,20 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            'ROFL': 'Bir şakaya karşılık cevap',
+            'SHEESH': 'Onaylamamak',
+            'CREEPY': 'Korkunç',
+            'AGGRO': 'Agresifleşmek'
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
 
+if word in meme_dict.keys():
+    # Kelime eşleşiyorsa ne yapmalıyız?
+    print(meme_dict[word])
+    
+else:
+    # Kelime eşleşmiyorsa ne yapmalıyız?
+    print('Im sorry but i dont know that word yet!')
 
   
